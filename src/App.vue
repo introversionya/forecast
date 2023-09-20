7a78fa3c5f68abc6afec2cb41246fb109819db70
Anton Demidenko <yaintroversivnost@gmail.com>
2023-09-20 12:45:41 +0500

    Обработка ошибок ввода

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
 <script setup>
-import { ref, onMounted } from 'vue';
-import { API_KEY, BASE_URL } from './constants/index'
+import { ref, onMounted, computed } from 'vue';
+import { API_KEY, BASE_URL } from './constants/index';
+import { capitalizeFirstLetter } from './utils';
 import WeatherSummary from './components/WeatherSummary.vue';
-import Highlights from './components/Highlights.vue'
+import Highlights from './components/Highlights.vue';
 import Coords from './components/Coords.vue';
 import Humidity from './components/Humidity.vue';
 
-const city = ref('Paris')
-const weatherInfo = ref(null)
+const city = ref('Paris');
+const weatherInfo = ref(null);
+const isError = computed(() => weatherInfo.value?.cod !== 200);
 
 function getWeather() {
   fetch(`${BASE_URL}?q=${city.value}&units=metric&appid=${API_KEY}`)
     .then((response) => response.json())
-    .then((data) => weatherInfo.value = data)
+    .then((data) => (weatherInfo.value = data));
 }
 
-onMounted(getWeather)
+onMounted(getWeather);
 </script>
 
 <template>
@@ -24,7 +26,7 @@ onMounted(getWeather)
       <div class="container">
         <div class="laptop">
           <div class="sections">
-            <section class="section section-left">
+            <section :class="['section', 'section-left', { 'section-error': isError }]">
               <div class="info">
                 <div class="city-inner">
                   <input
@@ -34,14 +36,35 @@ onMounted(getWeather)
                     @keyup.enter="getWeather"
                   />
                 </div>
-                <WeatherSummary :weatherInfo="weatherInfo" />
+                <WeatherSummary
+                  v-if="!isError"
+                  :weatherInfo="weatherInfo"
+                />
+                <div
+                  v-else
+                  class="error"
+                >
+                  <div class="error-title">Oooops! Something went wrong</div>
+                  <div
+                    v-if="weatherInfo?.message"
+                    class="error-message"
+                  >
+                    {{ capitalizeFirstLetter(weatherInfo?.message) }}
+                  </div>
+                </div>
               </div>
             </section>
-            <section class="section section-right">
+            <section
+              v-if="!isError"
+              class="section section-right"
+            >
               <Highlights :weatherInfo="weatherInfo" />
             </section>
           </div>
-          <div v-if="weatherInfo?.weather" class="sections">
+          <div
+            v-if="!isError"
+            class="sections"
+          >
             <Coords :coord="weatherInfo.coord" />
             <Humidity :humidity="weatherInfo.main.humidity" />
           </div>
@@ -82,6 +105,11 @@ onMounted(getWeather)
 
   @media (max-width: 767px)
     width: 100%
+    padding-right: 0
+
+  &.section-error
+    min-width: 235px
+    width: auto
     padding-right: 0
 
 .section-right
@@ -134,4 +162,15 @@ onMounted(getWeather)
 
   @media (max-width: 767px)
     width: 100%
+
+.error
+  padding-top: 20px
+
+  &-title
+    font-size: 18px
+    font-weight: 700
+
+  &-message
+    padding-top: 10px
+    font-size: 15px
 </style>
